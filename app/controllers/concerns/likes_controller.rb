738d14a98f1111c0ee463607be730fb30f31786f
Danillo <danillo.targino@gmail.com>
2022-04-26 11:08:39 -0400

    create likes

---
+++ app/controllers/concerns/likes_controller.rb
@@ -0,0 +1,17 @@
+class LikesController < ApplicationControll
+    def create
+        like = current_user.likes.build(like_params)
+
+        if like.save
+            render json: { successful: true, id:like.id }
+        else
+            render json: { successful: false }
+        end
+    end
+
+    private
+    
+    def like_params
+        params.require(:like).permit(:post_id)
+    end
+end
